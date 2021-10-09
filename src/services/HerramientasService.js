437050e45db0a86e6b4b7268780e022400529855
Erika <honorable.princesa@gmail.com>
2021-10-09 12:17:52 -0500

    [Herramientas] Se agrega service para exportar a CSV

--- src/services/HerramientasService.js
+++ src/services/HerramientasService.js
@@ -3,30 +3,30 @@ import HerramientasRepo from '../repos/HerramientasRepo';
 
 export default class herramientas {
     exportarCsv(columns, rows, nombreArchivo) {
-            // naive encoding to csv format
-            const content = [columns.map(col => HerramientasRepo.wrapCsvValue(col.labelExcel))].concat(
-                rows.map(row => columns.map(col => HerramientasRepo.wrapCsvValue(
-                  typeof col.field === 'function'
-                    ? col.field(row)
-                    : row[ col.field === void 0 ? col.name : col.field ],
-                  col.format
-                )).join(','))
-              ).join('\r\n')
-      
-              const status = exportFile(
-                `${nombreArchivo}.csv`,
-                content,
-                'text/csv'
-              )
-      
-              if (status !== true) {
-                $q.notify({
-                  message: 'Browser denied file download...',
-                  color: 'negative',
-                  icon: 'warning'
-                })
-              }
-        }
+      // naive encoding to csv format
+      const content = [columns.map(col => HerramientasRepo.wrapCsvValue(col.labelExcel))].concat(
+        rows.map(row => columns.map(col => HerramientasRepo.wrapCsvValue(
+          typeof col.field === 'function'
+            ? col.field(row)
+            : row[ col.field === void 0 ? col.name : col.field ],
+          col.format
+        )).join(','))
+      ).join('\r\n')
+
+      const status = exportFile(
+        `${nombreArchivo}.csv`,
+        content,
+        'text/csv'
+      )
+
+      if (status !== true) {
+        $q.notify({
+          message: 'Browser denied file download...',
+          color: 'negative',
+          icon: 'warning'
+        })
+      }
+  }
 }
     
 
