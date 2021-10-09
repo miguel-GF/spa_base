f66610dbdff6235416cb1a32013f3ff629de462b
Erika <honorable.princesa@gmail.com>
2021-10-09 12:12:08 -0500

    [Routes] Se agregan rutas básicas

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,7 +6,11 @@ const routes = [
     children: [
       { path: '', name: "home", component: () => import('pages/Index.vue') },
       { path: '/test', name: 'testing', component: () => import('pages/Prueba.vue') },
-      { path: '/inventario', name: 'inventario', component: () => import('pages/inventario/InventarioIndex.vue') },
+      { path: '/productos', name: 'productos', component: () => import('pages/productos/ProductosIndex.vue') },
+      { path: '/productos/agregar', name: 'productosAgregar', component: () => import('pages/productos/ProductosAgregar.vue') },
+      // Alumnos
+      { path: '/alumnos', name: 'alumnos', component: () => import('pages/alumnos/AlumnosIndex.vue') },
+      { path: '/alumnos/agregar', name: 'alumnosAgregar', component: () => import('pages/alumnos/AlumnosAgregar.vue') },
     ]
   },
 
