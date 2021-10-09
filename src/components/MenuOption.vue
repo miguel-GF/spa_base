daab7e0db1a2724f0942c9bce2f029ec8e9ea3f9
Erika <honorable.princesa@gmail.com>
2021-10-09 12:15:27 -0500

    [Menu] Se agregan componentes de menu

--- src/components/MenuOption.vue
+++ src/components/MenuOption.vue
@@ -1,63 +1,95 @@
 <template>
   <q-item
+    v-for="(dato, index) in datos"
+    :key="index"
     clickable
-    :class="rutaActiva"
-    @click="$router.push(link)"
+    @click="mostrarSubmenu(dato)"
     dense
-    class="q-py-xs"
+    class="q-py-xs"    
+    :class="dato.rutaNombre == storage.menu ? 'menu-option-active' :  'menu-option-no-active'"    
   >
     <q-item-section
-      v-if="icon"
+      v-if="dato.icon"
       avatar
     >
-      <q-icon :name="icon" />
+      <q-icon :name="dato.icon" />
     </q-item-section>
 
+    
     <q-item-section>
-      <q-item-label><span class="titulo">{{ title }}</span></q-item-label>
-      <q-item-label caption>
-        <span class="subtitulo">{{ caption }}</span>
-      </q-item-label>
+      <q-item-label><span class="titulo">{{ dato.title }}</span></q-item-label>
     </q-item-section>
   </q-item>
+  
+  <transition 
+    appear
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut"      
+    v-if="showSubmenu"
+  >
+    <submenu-option
+      :opciones="opcionesSubmenu"
+      @mouseleave="showSubmenu = false"
+      @cerrar="showSubmenu = false"
+    />
+  </transition>
+
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import SubmenuOptionVue from './SubmenuOption.vue'
 
 export default defineComponent({
   name: 'MenuOption',
   props: {
-    title: {
-      type: String,
-      required: true
+    datos: {
+      type: Array,
+      required: true,      
     },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    rutaNombre: {
-      type: String,
-      default: ''
-    },
-
-    icon: {
-      type: String,
-      default: ''
+  },
+  components: {
+    "submenu-option": SubmenuOptionVue,
+  },
+  // computed: {
+  //   rutaActiva(dato) {      
+  //     let estilo;
+  //     dato.rutaNombre == this.$route.name ? estilo = "menu-option-active" :  estilo = "";
+  //     return estilo;
+  //   }
+  // }
+  data() {
+    return {
+      showSubmenu: false,
+      opcionesSubmenu: [],
+      storage: '',
+      random: 0,
     }
   },
-  computed: {
-    rutaActiva() {
-      let estilo;
-      this.rutaNombre == this.$route.name ? estilo = "menu-option-active" :  estilo = "";
-      return estilo;
+  mounted() {
+    this.storage = JSON.parse(localStorage.getItem("menu"));
+  },
+  methods: {
+    mostrarSubmenu(dato) {
+      this.setMenuOptionActive(dato.rutaNombre);
+      this.storage = JSON.parse(localStorage.getItem("menu"));
+      this.random = Math.random() * (200000 - 1) + 1;
+
+      if(dato.rutaNombre == 'home') {
+        this.showSubmenu = false;        
+        this.$router.push('/');
+      }
+      else {
+        this.opcionesSubmenu = dato.opciones;
+        this.showSubmenu = true;        
+      }
+    },
+    setMenuOptionActive(menu, submenu = '') {
+      let data = {
+        menu,
+        submenu,
+      };
+      localStorage.setItem("menu", JSON.stringify(data));
     }
   }
 })
---
+++ src/components/SubmenuOption.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="zi-100 submenu">
+    <q-list>
+        <q-item :class="opcion.rutaNombre == this.$route.name ? 'submenu-option-active' :  ''"  clickable dense 
+            class="q-py-xs" @click="irRuta(opcion)"
+            v-for="(opcion, index) in opciones" :key="index">
+            <q-item-label class="titulo q-my-auto">
+                {{ opcion.title || '--' }}
+            </q-item-label>
+        </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MenuOption',
+  props: {
+    opciones: {
+      type: Array,
+      required: true
+    },  
+  },
+  data() {
+    return {
+      showSubmenu: false,
+    }
+  },
+  methods: {
+    irRuta(opcion) {      
+      this.$emit('cerrar');
+      this.$router.push(opcion.link);
+    }
+  }
+})
+</script>
